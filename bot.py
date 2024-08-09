f7fb14e5df0014e17155b1bb55586accf83ce6a7
Emperor-One <hamster@key-generator.key>
2024-08-10 02:51:17 +0300

    Pushed Files from local

---
+++ bot.py
@@ -0,0 +1,147 @@
+import subprocess
+import os
+import logging
+import asyncio
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import server
+
+# Paste Token Here if you don't wanna put it in an env. variable for some reason
+TOKEN_INSECURE = ""
+
+if os.name == 'posix':
+    TOKEN = subprocess.run(["printenv", "HAMSTER_BOT_TOKEN"], text=True, capture_output=True).stdout.strip()
+elif os.name == 'nt':
+    TOKEN = subprocess.run(["echo", "%HAMSTER_BOT_TOKEN%"], text=True, capture_output=True, shell=True).stdout.strip()
+    TOKEN = "" if TOKEN == "%HAMSTER_BOT_TOKEN%" else TOKEN
+
+
+AUTHORIZED_USERS = []
+EXCLUSIVE = False
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.CRITICAL
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Should make this a Database probably
+    # with open('user_ids','a') as file:
+    #     file.write(f"{update.effective_chat.first_name} : {update.effective_chat.id}\n")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="The Commands are:\n*/bike*\n*/clone*\n*/cube*\n*/train*\n*/all*\nThese will generate 4 keys for their respective games\.", parse_mode='MARKDOWNV2')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="You can also set how many keys are generated\. For example, */cube 8* will generate *EIGHT* keys for the cube game\.", parse_mode='MARKDOWNV2')
+
+
+async def bike(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
+        return
+    
+    server.logger.info(f"Generating for client: {update.effective_chat.id}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Generating\.\.\.", parse_mode='MARKDOWNV2')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"This will only take a moment\.\.\.", parse_mode='MARKDOWNV2')
+
+    no_of_keys = int(context.args[0]) if context.args else 4
+    keys = await server.run(chosen_game=1, no_of_keys=no_of_keys)
+    generated_keys = [f"`{key}`" for key in keys]
+    formatted_keys = '\n'.join(generated_keys)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{formatted_keys}", parse_mode='MARKDOWNV2')
+    server.logger.info("Message sent to the client.")
+
+async def clone(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
+        return
+    
+    server.logger.info(f"Generating for client: {update.effective_chat.id}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Generating\.\.\.", parse_mode='MARKDOWNV2')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"This will only take a moment\.\.\.", parse_mode='MARKDOWNV2')
+
+    no_of_keys = int(context.args[0]) if context.args else 4
+    keys = await server.run(chosen_game=2, no_of_keys=no_of_keys)
+    generated_keys = [f"`{key}`" for key in keys]
+    formatted_keys = '\n'.join(generated_keys)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{formatted_keys}", parse_mode='MARKDOWNV2')
+    server.logger.info("Message sent to the client.")
+
+async def cube(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
+        return
+    
+    server.logger.info(f"Generating for client: {update.effective_chat.id}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Generating\.\.\.", parse_mode='MARKDOWNV2')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"This will only take a moment\.\.\.", parse_mode='MARKDOWNV2')
+
+    no_of_keys = int(context.args[0]) if context.args else 4
+    keys = await server.run(chosen_game=3, no_of_keys=no_of_keys)
+    generated_keys = [f"`{key}`" for key in keys]
+    formatted_keys = '\n'.join(generated_keys)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{formatted_keys}", parse_mode='MARKDOWNV2')
+    server.logger.info("Message sent to the client.")
+
+async def train(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
+        return
+    
+    server.logger.info(f"Generating for client: {update.effective_chat.id}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Generating\.\.\.", parse_mode='MARKDOWNV2')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"This will only take a moment\.\.\.", parse_mode='MARKDOWNV2')
+
+    no_of_keys = int(context.args[0]) if context.args else 4
+    keys = await server.run(chosen_game=4, no_of_keys=no_of_keys)
+    generated_keys = [f"`{key}`" for key in keys]
+    formatted_keys = '\n'.join(generated_keys)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{formatted_keys}", parse_mode='MARKDOWNV2')
+    server.logger.info("Message sent to the client.")
+        
+
+async def all(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
+        return
+    
+    server.logger.info(f"Generating for client: {update.effective_chat.id}")
+    server.logger.info(f"Generating keys for All Games\.")
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Currently generating for all games\.\.\.", parse_mode='MARKDOWNV2')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Come Back in about 5\-10 minutes\.", parse_mode='MARKDOWNV2')
+
+
+    no_of_keys = int(context.args[0]) if context.args else 4
+    server.logger.info(f"Number of keys set to {no_of_keys}. Context args was {'not empty' if context.args else 'empty'}")
+    for i in range(4):
+        keys = await server.run(chosen_game=i+1, no_of_keys=no_of_keys)
+        generated_keys = [f"`{key}`" for key in keys]
+        formatted_keys = '\n'.join(generated_keys)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{formatted_keys}", parse_mode='MARKDOWNV2')
+        server.logger.info("Message sent to the client.")    
+    
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN or TOKEN_INSECURE).build()
+    server.logger.info("Server is running. Awaiting users...")
+
+    start_handler = CommandHandler('start', start, block=False)
+    application.add_handler(start_handler)
+
+    bike_handler = CommandHandler('bike', bike, block=False)
+    application.add_handler(bike_handler)
+
+    clone_handler = CommandHandler('clone', clone, block=False)
+    application.add_handler(clone_handler)
+
+    cube_handler = CommandHandler('cube', cube, block=False)
+    application.add_handler(cube_handler)
+
+    train_handler = CommandHandler('train', train, block=False)
+    application.add_handler(train_handler)
+
+    all_handler = CommandHandler('all', all, block=False)
+    application.add_handler(all_handler)
+
+
+    application.run_polling()
---
+++ server.py
@@ -0,0 +1,190 @@
+import asyncio
+import os
+import sys
+import httpx
+import random
+import time
+import uuid
+import datetime
+from loguru import logger
+
+# Disable logging for httpx
+httpx_log = logger.bind(name="httpx").level("WARNING")
+logger.remove()
+logger.add(sink=sys.stdout, format="<white>{time:YYYY-MM-DD HH:mm:ss}</white>"
+                                   " | <level>{level: <8}</level>"
+                                   " | <cyan><b>{line}</b></cyan>"
+                                   " - <white><b>{message}</b></white>")
+logger = logger.opt(colors=True)
+
+GAMES = {
+    1: {
+        'name': 'Riding Extreme 3D',
+        'appToken': 'd28721be-fd2d-4b45-869e-9f253b554e50',
+        'promoId': '43e35910-c168-4634-ad4f-52fd764a843f',
+    },
+    2: {
+        'name': 'My Clone Army',
+        'appToken': '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
+        'promoId': 'fe693b26-b342-4159-8808-15e3ff7f8767',
+    },
+    3: {
+        'name': 'Chain Cube 2048',
+        'appToken': 'd1690a07-3780-4068-810f-9b5bbf2931b2',
+        'promoId': 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+    },
+    4: {
+        'name': 'Train Miner',
+        'appToken': '82647f43-3f87-402d-88dd-09a90025313f',
+        'promoId': 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+    }
+}
+
+BASE_URL = 'https://api.gamepromo.io'
+EVENTS_DELAY = 30
+HTTPX_TIMEOUT = 30
+
+key_count = 0
+
+async def login(client_id, app_token):
+    headers = {
+        "User-Agent": "UnityPlayer/2022.3.20f1 (UnityWebRequest/1.0, libcurl/8.5.0-DEV)",
+        "Connection": "close"
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f'{BASE_URL}/promo/login-client',
+            headers=headers,
+            json={'appToken': app_token, 'clientId': client_id, 'clientOrigin': 'android'},
+            timeout=httpx.Timeout(HTTPX_TIMEOUT)  
+        )
+        response.raise_for_status()
+        client_token = response.json()['clientToken']
+        logger.info(f"Logged in with Client Token: {client_token}")
+
+        return client_token
+
+async def register_event(client_token, promo_id):
+    headers = {
+        "Authorization": f"Bearer {client_token}",
+        "User-Agent": "UnityPlayer/2022.3.20f1 (UnityWebRequest/1.0, libcurl/8.5.0-DEV)",
+        "Connection": "close"
+    }
+    async with httpx.AsyncClient() as client:
+        while True:
+            delay_time = EVENTS_DELAY * (random.uniform(0,0.33) + 1)
+            logger.info(f"Sleeping for {delay_time} seconds.")
+            await asyncio.sleep(delay_time)
+
+            response = await client.post(
+                f'{BASE_URL}/promo/register-event',
+                headers=headers,
+                json={'promoId': promo_id, 'eventId': str(uuid.uuid4()), 'eventOrigin': 'undefined'},
+                timeout=httpx.Timeout(HTTPX_TIMEOUT)  
+            )
+            logger.info(f"Response received: {response.json()}")
+
+            if 'hasCode' in response.json():
+                break
+
+        has_code = response.json()['hasCode']
+
+        if has_code:
+            logger.success("Code is ready!")
+        else:
+            logger.info("Code is not ready.")
+        return has_code
+
+
+async def create_code(client_token, promo_id):
+    headers = {
+        "Authorization": f"Bearer {client_token}",
+        "User-Agent": "UnityPlayer/2022.3.20f1 (UnityWebRequest/1.0, libcurl/8.5.0-DEV)",
+        "Connection": "close"
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f'{BASE_URL}/promo/create-code',
+            headers=headers,
+            json={'promoId': promo_id},
+            timeout=httpx.Timeout(HTTPX_TIMEOUT)
+        )
+        response.raise_for_status()
+        key = response.json()['promoCode']
+
+        logger.success(f"Key Generated: {key}")
+        return key
+
+
+async def play_the_game(app_token, promo_id):
+    client_id = str(uuid.uuid4())
+
+    try:
+        client_token = await login(client_id, app_token)
+
+    except Exception as e:
+        logger.error(f"Failed to login: {e}")
+        input("Press Any Key To Exit")
+
+    for _ in range(11):
+        try:
+            has_code = await register_event(client_token, promo_id)
+        except Exception as e:
+            return None
+
+
+        if has_code:
+            break
+
+    try:
+        key = await create_code(client_token, promo_id)
+        
+        return key
+
+    except Exception as e:
+        logger.error(f"An error occured while trying to create the code: {e}")
+        return None
+
+async def main(chosen_game, no_of_keys):
+    tasks = [play_the_game(GAMES[chosen_game]['appToken'], GAMES[chosen_game]['promoId']) for _ in range(no_of_keys)]
+    keys = await asyncio.gather(*tasks)
+    return [key for key in keys if key]
+
+
+# Call run directly if you are a bot
+async def run(chosen_game, no_of_keys):
+    if no_of_keys == 1:
+        logger.info(f"Generating {no_of_keys} key for {GAMES[chosen_game]['name']}")
+    else:
+        logger.info(f"Generating {no_of_keys} keys for {GAMES[chosen_game]['name']}")
+
+    keys = await main(chosen_game=chosen_game, no_of_keys=no_of_keys)    
+    return keys
+    
+if __name__ == "__main__":
+    print("Select a game:")
+    for key, value in GAMES.items():
+        print(f"{key}: {value['name']}")
+    chosen_game = int(input("Enter the game number: "))
+    no_of_keys = int(input("Enter the number of keys to generate: "))
+
+    if no_of_keys == 1:
+        logger.info(f"Generating {no_of_keys} key for {GAMES[chosen_game]['name']}")
+    else:
+        logger.info(f"Generating {no_of_keys} keys for {GAMES[chosen_game]['name']}")
+   
+    keys = asyncio.run(main(chosen_game, no_of_keys))
+
+    if keys:
+        with open('Keys.txt', 'a') as file:  
+            for key in keys:
+                file.write(f"{key}\n")
+            logger.success("Generated Key(s) were successfully saved to Keys.txt")
+
+    else:
+        logger.error("No keys were generated.")    
+
+    input("Press Any Key To Exit")
+
